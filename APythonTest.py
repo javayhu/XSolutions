bbec8a3594adfe0016bab93a0e4a88772641870c
hujiaweibujidao <hujiawei090807@gmail.com>
2014-10-08 16:01:20 +0800

    delete useless files

--- APythonTest.py
+++ APythonTest.py
@@ -98,6 +98,23 @@ import numpy as np
 max_speeds = np.load('intro/summary-exercises/examples/max-speeds.npy')
 years_nb = max_speeds.shape[0]
 
+import os
+def tree(top):
+    #path,folder list,file list
+    for path, names, fnames in os.walk(top):
+        for fname in fnames:
+            # yield os.path.join(path, fname)
+            yield fname
+
+for name in tree(os.getcwd()):
+    print name
+
+for name in tree('/Users/hujiawei/Desktop/csu/'):
+        print '![image](http://hujiaweibujidao.github.io/images/pics/'+name+')'
+
+if __name__ == '__main__':
+
+    pass
 
 
 
